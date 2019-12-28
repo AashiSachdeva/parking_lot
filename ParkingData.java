dbe3e9f749e16934e2625a519551b21f8c10eb98
aashisachdeva <aashisachdeva18@gmail.com>
2019-12-28 19:05:14 +0530

    Interactive

---
+++ ParkingData.java
@@ -0,0 +1,14 @@
+class ParkingData{
+    private int slotNumber;
+    private String registrationNumber, colour;
+    ParkingData(int slotNumber, String registrationNumber,String colour){
+     
+        this.slotNumber = slotNumber;
+        this.registrationNumber = registrationNumber;
+        this.colour = colour;
+    }
+
+    public String getDetails(){
+        return this.slotNumber+"\t\t"+this.registrationNumber+"\t\t"+this.colour;
+    }
+}
--- parking_lot.sh
+++ parking_lot.sh
@@ -1,2 +1,2 @@
-javac  main.java
+javac main.java
 java main
